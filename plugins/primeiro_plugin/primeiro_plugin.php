dca51b3c2663b598127a0409129376ddcdf03368
Sayuri Hioki <sayuri.hioki12@gmail.com>
2020-11-10 16:33:03 -0300

    aula 27/10

--- plugins/primeiro_plugin/primeiro_plugin.php
+++ plugins/primeiro_plugin/primeiro_plugin.php
@@ -14,3 +14,21 @@ add_filter('login_errors', 'altera_msg_login');
 function altera_msg_login(){
     return 'Credenciais inválidas, tente novamente';
 }
+
+add_action('wp_head', 'colocar_og_tags');
+
+function colocar_og_tags(){
+    if(is_single()){
+        $dados_post = get_post(get_the_ID());
+        $nome_site = get_bloginfo();
+        $titulo = $dados_post->post_title;
+        $resumo = $dados_post->post_excerpt;
+        
+        echo "\n\n\n
+            <meta property='og:title' content='" . $titulo . "'>\n
+            <meta property='og:site_name' content='" . $nome_site . "'>\n
+            <meta property='og:url' content='" . get_permalink() . "'>\n
+            <meta property='og:description' content='" . $resumo . "'>\n
+            \n\n\n";
+    }
+}
